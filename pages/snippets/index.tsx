5f498249aa8bd801e4f89890468c956aa723e7bd
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2022-03-19 20:58:58 -0300

    feat: doppler snippet

--- pages/snippets/index.tsx
+++ pages/snippets/index.tsx
@@ -10,17 +10,15 @@ export default function Snippets({
   return (
     <Container
       title="Code Snippets – Gabriel Pan Gantes"
-      description="A collection of code snippets – including serverless functions, Node.js scripts, and CSS tricks."
+      description="A collection of code snippets – including serverless functions, Node.js scripts, and infrascture as a code solutions."
     >
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
           Code Snippets
         </h1>
         <p className="mb-4 text-gray-600 dark:text-gray-400">
-          These are a collection of code snippets I've used in the past and
-          saved. Some are Serverless Functions, which include set up
-          instructions. Others are anything from random CSS snippets to Node.js
-          scripts.
+          The following snippets are some of the solutions I've made for different projects I've worked on.
+          Some are serverless functions, pipelines tasks, Node.js scripts, and infrastructure as code solutions.
         </p>
         <div className="grid w-full grid-cols-1 gap-4 my-2 mt-4 sm:grid-cols-2">
           {snippets.map((snippet) => (
