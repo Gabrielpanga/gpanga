2f1579f3fba351a4a21db8b4d29dd39307450c89
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2022-11-08 23:52:39 -0300

    feat: use planetscale to store email subscribers

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 - `data/*` - MDX data that is used for blogs, newsletters, and code snippets.
 - `layouts/*` - The different page layouts each MDX category (blog, newsletter, snippets) uses.
 - `lib/*` - Short for "library", a collection of helpful utilities or code for external services.
-- `pages/api/*` - [API routes](https://nextjs.org/docs/api-routes/introduction) powering [`/dashboard`](https://gpanga.com/dashboard), newsletter subscription, guestbook, and post views.
+- `pages/api/*` - [API routes](https://nextjs.org/docs/api-routes/introduction) powering [`/dashboard`](https://gpanga.com/dashboard), newsletter subscription, and post views.
 - `pages/blog/*` - Static pre-rendered blog pages using MDX.
 - `pages/dashboard` - [Personal dashboard](https://gpanga.com/dashboard) tracking metrics.
 - `pages/*` - All other static pages.
--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,12 +1,13 @@
+import prisma from 'lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (!process.env.REVUE_API_KEY) {
-    return res.status(200).json({ error: 'getrevue.co was not configured' });
-  }
+  // if (!process.env.REVUE_API_KEY) {
+  //   return res.status(200).json({ error: 'getrevue.co was not configured' });
+  // }
 
   const { email } = req.body;
 
@@ -14,19 +15,34 @@ export default async function handler(
     return res.status(400).json({ error: 'Email is required' });
   }
 
-  const result = await fetch('https://www.getrevue.co/api/v2/subscribers', {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${process.env.REVUE_API_KEY}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email })
-  });
-  const data = await result.json();
 
-  if (!result.ok) {
-    return res.status(500).json({ error: data.error.email[0] });
+  if (req.method === 'POST') {
+    const newEntry = await prisma.subscribers.create({
+      data: {
+        email,
+      }
+    });
+
+    return res.status(200).json({
+      id: newEntry.id.toString(),
+      updated_at: newEntry.updated_at
+    });
   }
 
-  return res.status(201).json({ error: '' });
+  // TODO: Waiting for getrevue api key
+  // const result = await fetch('https://www.getrevue.co/api/v2/subscribers', {
+  //   method: 'POST',
+  //   headers: {
+  //     Authorization: `Token ${process.env.REVUE_API_KEY}`,
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({ email })
+  // });
+  // const data = await result.json();
+
+  // if (!result.ok) {
+  //   return res.status(500).json({ error: data.error.email[0] });
+  // }
+
+  // return res.status(201).json({ error: '' });
 }
