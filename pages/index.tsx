d8f3e98530e765802faf022176a9a25175cdc8df
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2023-10-10 15:02:44 +0200

    feat: add blog to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,16 +50,10 @@ export default function Home({ videos }) {
             gradient="from-[#D8B4FE] to-[#818CF8]"
           />
           <BlogPostCard
-            title="Beautiful Kafka implementation on Serverless Stack"
-            slug=""
-            releaseDate='2022-04-15'
+            title="How to create your company's second mind using supabase's pgvector?"
+            slug="supabase-openai-second-brain"
+            releaseDate='2023-10-10'
             gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
-          />
-          <BlogPostCard
-            title="Fire your releases from Slack"
-            releaseDate='2022-04-25'
-            slug=""
-            gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
           />
         </div>
         <Link href="/blog">
